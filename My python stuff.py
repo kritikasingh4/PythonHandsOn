ce845a9fba0e8eb871971039b12163d61d1ed7f4
kritikasingh4 <106058845+kritikasingh4@users.noreply.github.com>
2022-05-23 17:46:03 +0530

    Add files via upload

--- My python stuff.py
+++ My python stuff.py
@@ -346,3 +346,317 @@ exponent=int(input("Enter the exponent value: "))
 result=pow(base, exponent)
 print("Answer is:"+str(result))
 
+
+# # ACTIVITY 3
+# 
+
+# # 1. display powers of 2 using anynymous function
+
+# In[2]:
+
+
+terms=int(input("Enter the number of terms: "))
+result=list(map(lambda x:2**x, range(terms)))
+print("Total terms are: ",terms)
+for i in range(terms):
+    print("2 power",i,"is",result[i])
+
+
+# # 2. find numbers divisible by other numbers
+
+# In[3]:
+
+
+mylist=[13, 34, 65, 107, 117, 130]
+result=list(filter(lambda x:(x%13==0), mylist))
+print("Numbers divisible by 13 are: ",result)
+
+
+# # 3. Convert Decimal to Binary, Octal and Hexadecimal
+
+# In[6]:
+
+
+dec=int(input("Enter a decimal value: "))
+print(bin(dec),"in binary.")
+print(oct(dec),"in octal.")
+print(hex(dec),"in hexadecimal.")
+
+
+# # 4. find ASCII value of character
+
+# In[10]:
+
+
+c = 'p'
+print("The ASCII value of '" + c + " is", ord(c))
+
+
+# # 5. find HCF or GCD
+
+# In[13]:
+
+
+def compute_hcf(x,y):
+    while(y):
+        x,y=y,x%y
+        return x
+hcf=compute_hcf(300,400)
+print("The HCF is",hcf)
+
+
+# # 6. find LCM
+
+# In[23]:
+
+
+def compute_lcm(x, y):
+
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+print("The L.C.M. is", compute_lcm(num1, num2))
+
+
+# # 7. find factors of a number
+
+# In[30]:
+
+
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = int(input("Enter the number to find its factors:"))
+
+print_factors(num)
+
+
+# # 8. simple calculator
+
+# In[2]:
+
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+            break 
+          
+    
+    else:
+        print("Invalid Input")
+
+
+# # 9. shuffle a deck of cards
+
+# In[3]:
+
+
+import itertools, random
+deck=list(itertools.product(range(1,14),['spade', 'heart', 'diamond', 'club']))
+
+random.shuffle(deck)
+
+print("You got:")
+for i in range(5):
+    print(deck[i][0], "of", deck[i][1])
+
+
+# # 10. display calendar
+
+# In[6]:
+
+
+import calendar
+
+yy=int(input("Enter year: "))
+mm=int(input("Enter month: "))
+
+print(calendar.month(yy,mm))
+
+
+# # 11. fibonacci series using recursion
+
+# In[8]:
+
+
+def recur_fibo(n):
+    
+    if n<=1:
+        return n
+    else:
+        return( recur_fibo(n-1)+recur_fibo(n-2))
+    
+nterms=5
+
+if nterms<=0:
+    print("Please enter a positive integer: ")
+else:
+    print("Fibonacci series: ")
+    for i in range(nterms):
+        print(recur_fibo(i))
+
+
+# # 12. sum of natural numbers using recursion
+
+# In[10]:
+
+
+def recur_sum(n):
+    if n<=0:
+        return n
+    else:
+        return n+ recur_sum(n-1)
+    
+num=int(input("Enter a number:"))
+
+if num<0:
+    print("Enter a positive number please.")
+
+else:
+    print("The sum is: ", recur_sum(num))
+
+
+# # 13. find factorial using recursion
+
+# In[22]:
+
+
+def recur_factorial(n):
+    if n==1:
+        return n
+    else:
+        return n*recur_factorial(n-1)
+
+num=int(input("Enter a number: "))
+
+if num<0:
+    print("Please enter a positive number.")
+    
+elif num==0:
+    print("Factorial of 0 is 1.")
+    
+else:
+    print("Factorial of", num, "is: ", recur_factorial(num))
+
+
+# # 14. convert decimal to binary using recursion
+
+# In[24]:
+
+
+def converttobinary(n):
+    if n>1:
+        converttobinary(n//2)
+    print(n%2, end=' ')
+    
+dec=int(input("Enter a decimal: "))
+
+converttobinary(dec)
+print()
+
+
+# # 15. return mutliple values from a function 
+
+# In[27]:
+
+
+def name():
+    n1=str(input("Enter a name: "))
+    n2=str(input("Enter another name: "))
+    n3=str(input("Enter a name: "))
+    
+    return{1:n1, 2:n2, 3:n3}
+names=name()
+print(names)
+
+
+# # ACTIVITY 4
+
+# # 1. sum of arrays
+
+# In[28]:
+
+
+arr=[]
+arr=[1,568,4566,9000,789]
+ans=sum(arr)
+print("The sum is: ", ans)
+
+
+# # 2. largest element in array
+
+# In[34]:
+
+
+def largest(arr,n):
+    max = arr[0]
+    for i in range(1, n):
+        if arr[i] > max:
+            max = arr[i]
+        return max
+
+arr = [10, 324, 45, 90, 9808]
+n = len(arr)
+Ans = largest(arr,n)
+print ("Largest in given array is",Ans)   
+
+
+# # 3. array rotation
+
+# In[ ]:
+
+
+
+
